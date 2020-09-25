3b0900bb11debe2c36b9fa4f153e937b9ce768f3
Pawel Kaminski <pawelkaminski22@gmail.com>
2020-09-25 21:08:16 +0200

    final version

--- main.py
+++ main.py
@@ -1,46 +1,64 @@
 from faker import Faker
-
 cards = []
 
 
 class BaseContact:
-    def __init__(self, name, last_name, telephone, email):
+    def __init__(self, name, last_name, phone, email):
         self.name = name
         self.last_name = last_name
         self.email = email
-        self.telephone = telephone
-        self._name_len = len(name) + len(last_name)
+        self.phone = phone
+        self._label_length = len(name) + len(last_name)
 
     @property
-    def name_len(self):
-        return self._name_len
+    def label_length(self):
+        return self._label_length
 
-    @name_len.setter
-    def name_len(self, value):
-        self._name_len = len(value)
-
-    def __str__(self):
-        return f'{self.name} {self.last_name} with email: {self.email}'
+    @label_length.setter
+    def label_length(self, value):
+        self._label_length = len(value)
 
     def contact(self):
-        return print(f'Kontaktuje sie z: {self.name} {self.last_name} pracuje jako {self.position} i posiada maila {self.email}')
+        return print(f'Wybieram numer: {self.phone} i dzwonie do {self.name} {self.last_name}')
+
+
+class BusinessContact(BaseContact):
+    def __init__(self, name, last_name, phone, email, position, company, work_phone):
+        super().__init__(name, last_name, phone, email)
+        self.position = position
+        self.company = company
+        self.work_phone = work_phone
+
+    def contact(self):
+        return print(f'Wybieram numer: {self.work_phone} i dzwonie do {self.name} {self.last_name}')
 
 
 if __name__ == '__main__':
     fake = Faker()
-    friend = Card(fake.first_name(), fake.last_name(), fake.job(), fake.email())
 
-    for i in range(3):
-        friend = Card(fake.first_name(), fake.last_name(), fake.job(), fake.email())
-        cards.append({'name': friend.name, 'last name': friend.last_name, 'email': friend.email})
-        print(friend.name_len)
-        print(friend.contact())
+    def create_contacts(card_type, quantity):
+        if card_type == 'base':
+            for i in range(quantity):
+                friend = BaseContact(fake.first_name(), fake.last_name(), fake.random_int(), fake.email())
+                cards.append({'name': friend.name, 'last name': friend.last_name, 'phone': friend.phone, 'email': friend.email})
+                print(friend.label_length)
 
-    s_cards = sorted(cards, key=lambda k: k['email'])
+        elif card_type == 'business':
+            for i in range(quantity):
+                friend = BusinessContact(fake.first_name(), fake.last_name(), fake.random_int(), fake.email(),
+                                         fake.job(), fake.company(), fake.random_int())
+                cards.append({'name': friend.name, 'last name': friend.last_name, 'phone': friend.phone, 'email': friend.email,
+                              'position': friend.position, 'company': friend.company, 'work phone': friend.work_phone})
+                print(friend.label_length)
 
+
+    s_cards = sorted(cards, key=lambda k: k['name'])
+
+    create_contacts('business', 2)
+    print(cards)
     #print(cards)
     #print(s_cards)
-    print(friend)
+    #print(friend)
 
 
 
